074e66be3f7884ccd73f7d0f4609fe6a1be0a778
Danny Shi <dannyshi.sr@gmail.com>
2020-08-18 16:16:44 -0400

    Updates

---
+++ precache-manifest.56c1f6a1efd72a39303556a789437b89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcc3c3154f62b73b67244a6ed297d925",
+    "url": "/SpeedTyper/index.html"
+  },
+  {
+    "revision": "676b322f87896a033da2",
+    "url": "/SpeedTyper/static/css/main.6af8db08.chunk.css"
+  },
+  {
+    "revision": "18c228c764d753be372e",
+    "url": "/SpeedTyper/static/js/2.e54ca885.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SpeedTyper/static/js/2.e54ca885.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "676b322f87896a033da2",
+    "url": "/SpeedTyper/static/js/main.78080248.chunk.js"
+  },
+  {
+    "revision": "288d71538144d48900bd",
+    "url": "/SpeedTyper/static/js/runtime-main.3e3f0e91.js"
+  }
+]);
